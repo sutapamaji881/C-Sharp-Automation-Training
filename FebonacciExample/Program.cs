95e5bd9f617709ef6cb0abc1200d413b5fb03229
Sutapa_Maji <Sutapa_Maji@EPINHYDW0894>
2021-06-03 14:32:37 +0530

    Add project files.

---
+++ FebonacciExample/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FebonacciExample
+{
+    public class FebonacciExample
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0;
+            int len = 7;
+            Console.Write("{0} {1}", a, b);
+            for (int i = 2; i < len; i++)
+            {
+                c = a + b;
+                Console.Write(" {0}", c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+    
+}
---
+++ ListExample/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListExample
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<String> users = new List<String>();
+
+            bool anotherUser = true;
+            int count=0;
+            while (anotherUser)
+
+            {
+                Console.WriteLine("Please specify a name.");
+                string userName = Console.ReadLine();
+               
+                users.Add(userName);
+                count++;
+                if (count == 4) {
+                    anotherUser = false;
+                }
+               
+
+            }
+            Console.WriteLine("\nNames of added users:");
+
+            foreach (String u in users)
+            {
+                Console.WriteLine(u);
+
+            }
+            string longest = "";
+           
+            foreach (string L in users)  
+            {
+
+                if (L.Length > longest.Length)
+                {
+                    longest = L;
+                }
+            
+            }
+            Console.Write("\nlongest name in the List" + longest);
+        }
+    }
+
+   
+    }
