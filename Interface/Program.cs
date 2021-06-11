06b73c49571193d5335bd6074ddddc4d3b11bd97
Sutapa_Maji <Sutapa_Maji@EPINHYDW0894>
2021-06-11 16:21:13 +0530

    Interface Assignment

---
+++ Interface/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Interface
+{
+    public interface ICollege
+    {
+        public void PrintDegree();
+    }
+    public interface ISchool
+    {
+        public void PrintSSCMarkSheet();
+    }
+    public class Student : ISchool,ICollege
+    {
+        public void PrintDegree()
+        {
+            Console.WriteLine("ICollege Implementation : Degree has printed and handed over");
+        }
+
+        public void PrintSSCMarkSheet()
+        {
+            Console.WriteLine("ISchool Implementation : Degree has printed and handed over");
+        }
+
+        class program
+        {
+            public static void Main(string[] args)
+            {
+                ISchool school = new Student();
+                school.PrintSSCMarkSheet();
+
+                ICollege college = new Student();
+                college.PrintDegree();
+            }
+        }
+    }
+
+}
