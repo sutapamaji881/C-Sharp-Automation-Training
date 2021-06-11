e69f154b2967c271901578200ce785a85e4f57e7
Sutapa_Maji <Sutapa_Maji@EPINHYDW0894>
2021-06-11 19:17:14 +0530

    Utility of Factorial and evn and odd logic

---
+++ Utility_Assignment2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Utility_Assignment2
+{
+    class Utility
+    {
+        public void EvenOdd(int a)
+        {
+            if (a / 2 == 0)
+            {
+                Console.WriteLine("Number is even");
+            }
+            else
+            {
+                Console.WriteLine("Number id Odd");
+            }
+        }
+
+        public void Factorial(int n)
+        {
+            int fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+        }
+        
+    }
+}
