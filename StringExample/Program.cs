610a3bd4419170023515d73084c6e8a2238e0f29
Sutapa_Maji <Sutapa_Maji@EPINHYDW0894>
2021-06-21 16:17:40 +0530

    String assignment

---
+++ StringExample/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace StringExample
+{
+    class Program
+    {
+
+        string name = "sutapa maji";
+        public void upperCase()
+        {
+            
+            string ucaseAuthor = name.ToUpper();
+            Console.WriteLine($"Uppercase: {ucaseAuthor}");
+
+        }
+
+        public void inputFnameLname()
+        {
+            Console.WriteLine("Enter your First Name");
+            String FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Last Name");
+            String LastName = Console.ReadLine();
+
+            Console.WriteLine("User Input : {0} {1} ", FirstName, LastName);
+        }
+
+        public void concatenateFnameLname()
+        {
+            var names = name.Split(' ');
+            string firstName = names[0];
+            string lastName = names[1];
+
+            Console.WriteLine("concatenate First name and Last name: " + firstName + lastName);
+        }
+
+        public static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.upperCase();
+            obj.inputFnameLname();
+            obj.concatenateFnameLname();
+        }
+    }
+}
