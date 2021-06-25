0961493923eac962d5333797fe25a87f145e6984
Sutapa_Maji <Sutapa_Maji@EPINHYDW0894>
2021-06-25 17:03:16 +0530

    LINQ Assignment

---
+++ LINQAssigment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQAssigment
+{
+    class LINQAssigment
+    {
+        List<String> cityNames = new List<string>();
+
+        public void GetFirstValidValue()
+        {
+            cityNames.Add("Bangalore");
+            cityNames.Add("Chennai");
+            cityNames.Add("Bangalore1");
+            cityNames.Add("Mumbai");
+
+            var result = cityNames.FirstOrDefault(myCity => myCity.Contains("Bangalore"));
+            Console.WriteLine(result);
+
+        }
+
+        public void GetValidCount()
+        {
+            var count = cityNames.Count();
+            Console.WriteLine(count);
+        }
+        static void Main(string[] args)
+        {
+            LINQAssigment obj = new LINQAssigment();
+            obj.GetFirstValidValue();
+            obj.GetValidCount();
+            
+        }
+    }
+}
