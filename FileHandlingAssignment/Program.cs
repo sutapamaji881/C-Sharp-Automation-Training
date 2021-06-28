cdade0d663115e55b4b7fa3c458797b84fc3eee0
Sutapa_Maji <Sutapa_Maji@EPINHYDW0894>
2021-06-28 16:08:56 +0530

    file handling assignment

--- FileHandlingAssignment/Program.cs
+++ FileHandlingAssignment/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.IO;
 
-namespace FileHandlingAssignment
+
+
+namespace Filehandling
 {
     class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+            //Task 1 :- Creation of directory using Directory info class
+            String path = @"C:\STUDENT";
+            DirectoryInfo fl = new DirectoryInfo(path);
+            fl.Create();
+            {
+                Console.WriteLine("Directory has been created");
+            }
+
+            //Task2 :- Creation of file in directory 
+            String Studentsname = "Studentsname.txt";
+            String filepath = Path.Combine(path, Studentsname);
+
+
+
+            Console.WriteLine("Enter student name");
+            Console.WriteLine("Enter Student Roll no");
+            StreamWriter sw = new StreamWriter(filepath);
+            sw.WriteLine(Console.ReadLine());
+            sw.Flush();
+            sw.Close();
+
         }
     }
+
+
+
+
 }
